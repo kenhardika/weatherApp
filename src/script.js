5e2435a5b9ced3449c5ed551828c84c02f43a130
kenhardika <kenzamahardika@gmail.com>
2022-08-08 20:00:33 +0700

    update showWeather to return as obj

--- src/script.js
+++ src/script.js
@@ -11,23 +11,56 @@ async function getWeather(city){
     try{
         let weatherAPI = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=a13d8edf32d7b9d01229e1749c07fcdc`);
         let responseAPI = await weatherAPI.json();
-        console.log(responseAPI);
+        return responseAPI
     }
     catch{
-        console.log('error loading the weather')
+        // console.log('error get weather')
+        throw new Error('error weather not found!')
     }
 }
 
 async function getForecast(city){
     try{
         let weatherAPI = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=a13d8edf32d7b9d01229e1749c07fcdc`);
-        let responseAPI = await weatherAPI.json();
-        console.log(responseAPI);
+        let responseAPI = await weatherAPI.json(); 
+        return responseAPI
     }
     catch{
-        console.log('error loading the weather')
+        throw new Error('error weather forecast not found!')
     }
 }
+async function showWeather(city){
+    let weatherCity = await getWeather(city);
+    let weatherForecast = await getForecast(city);
+    try { 
+        // console.log(weatherCity)
+        // console.log(weatherCity.weather[0].description);
+            const { 
+                clouds:{ all:cloud }, name, main:{temp}, wind:{speed} 
+            } = weatherCity;
+            const desc = weatherCity.weather[0].description;
+            
+            return{
+                name, 
+                cloud, 
+                temp, 
+                desc,
+                speed
+            }
+        }
+    catch{
+        //console.log(weatherCity.message + 'error code: '+ weatherCity.cod);
+        throw new Error(weatherCity.message); 
+    }
+    //console.log(weatherForecast)
+}
 
-getWeather('Cimahi');
-getForecast('Cimahi');
+showWeather('lokataralo')
+.then((val)=> {
+    console.log('city: ' + val.name)
+    console.log('temp: '+ val.temp + '°C')
+    console.log('description: ' + val.desc)
+    console.log('cloud percentage: '+ val.cloud + '%')
+    console.log('wind speed: '+ val.speed + ' kph')
+    })
+.catch((mes)=> console.log(mes));
